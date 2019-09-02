4532c5135e501388cc7eb0f3e9ba0badc69e796a
Tom Corke <tom@tomcorke.com>
2019-09-02 20:00:39 +0100

    Add stale-while-revalidate to API cache control header

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,6 @@ import { getData } from './data';
 
 export default async (req: NowRequest, res: NowResponse) => {
   const data = await getData()
-  res.setHeader('Cache-Control', 'maxage=0, s-maxage=60')
+  res.setHeader('Cache-Control', 'maxage=0, s-maxage=60, stale-while-revalidate')
   res.status(200).json(data)
 }
